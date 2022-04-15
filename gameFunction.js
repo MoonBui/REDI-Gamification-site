f2b98d8038d084715b66d4686d80ed1c16e3e5ea
Moon Bui <buinm@miamioh.edu>
2022-04-14 23:34:34 -0400

    new updates to gamification page

--- gameFunction.js
+++ gameFunction.js
@@ -7,7 +7,7 @@ function toggleIcon(n) {
     var icon = document.getElementsByTagName("i");
     icon[n].style.display = "none";
     results += 1;
-    document.getElementById("result").innerHTML = results;
+    // document.getElementById("result").innerHTML = results;
     if (results == MAX_NUM) {
         document.getElementById("myModal").style.display = "inline-flex";
     }
--- nameScript.py
+++ nameScript.py
@@ -1,7 +1,8 @@
 import os
 
-file_list =  os.listdir("E:\School related\REDI site\Grayscale Logos")
+file_list =  os.listdir("E:\School related\REDI site\Color Logos")
 for name in file_list:
-    print("<div class='investor-logo'>")
-    print(f'<img alt="{name}" class="lazyload"src="Grayscale Logos/{name}" alt="{name}">')
-    print("</div>")
+    print("<a class='logo-items'>")
+    print(f'<img alt="{name} (opens in a new tab)" data-src="./Color Logos/{name}" class="logo lazyload" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="><noscript><img class="logo" src="./Color Logos/{name}" alt="{name} (opens in a new tab)"></noscript>')
+    print("</a>")
+     
