907687f3b15091b3fc8c5ae8bc5ebab8cfb1605a
Moon Bui <buinm@miamioh.edu>
2022-04-18 23:27:02 -0400

    final changes to site

--- gameFunction.js
+++ gameFunction.js
@@ -4,10 +4,18 @@ const MAX_NUM = 4
 document.getElementById("myModal").style.display = "none";
 
 function toggleIcon(n) {
-    var icon = document.getElementsByTagName("i");
-    icon[n].style.display = "none";
+    var icon = document.getElementsByClassName("find-icon");
+    icon[n].style.opacity = '0'
+    window.setTimeout(
+        function removethis() {
+            icon[n].style.display = "none";
+        }, 300);
     results += 1;
-    // document.getElementById("result").innerHTML = results;
+
+    if (MAX_NUM / 2 == results) {
+        alert("Half way there! Doing Great!")
+    }
+
     if (results == MAX_NUM) {
         document.getElementById("myModal").style.display = "inline-flex";
     }
--- nameScript.py
+++ nameScript.py
@@ -2,7 +2,7 @@ import os
 
 file_list =  os.listdir("E:\School related\REDI site\Color Logos")
 for name in file_list:
-    print("<a class='logo-items'>")
+    print("<a class='logo-item'>")
     print(f'<img alt="{name} (opens in a new tab)" data-src="./Color Logos/{name}" class="logo lazyload" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="><noscript><img class="logo" src="./Color Logos/{name}" alt="{name} (opens in a new tab)"></noscript>')
     print("</a>")
      
